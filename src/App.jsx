ca1537e4c5eed61a571908b1f18a650302d5dbe8
mohammadasmailzada <mohammmadismailzada83@gmail.com>
2024-07-10 10:43:41 -0400

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg'; 
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
@@ -14,23 +12,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   
       <TodoList />
       <AddTodoForm onAddTodo={handleAddTodo} />
       <p>New Todo: {newTodo}</p>

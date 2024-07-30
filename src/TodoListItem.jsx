0d996243b15ad8d8f67cca67e7c65b3fc03bd5bf
mohammadasmailzada <mohammmadismailzada83@gmail.com>
2024-07-29 23:21:46 -0400

    complate

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -3,14 +3,7 @@ import React from 'react';
 function TodoListItem({ item }) {
 const myArray = ['apple', 'banana', 'orange'];
 
-  return (
-    <ul>
-      {item.text}
-      <li key={item.id}>{item.title}</li>
-      const myArray = ['apple', 'banana', 'orange'];
-
-    </ul>
-  );
+  return ;
 }
 
 export default TodoListItem
